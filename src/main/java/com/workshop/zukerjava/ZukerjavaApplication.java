959aa47f4a6fc9a9f2f5ba85ab2f18980f4f5e6f
yyff20000 <yuanfeng20000@gmail.com>
2018-11-21 21:33:19 +0800

    blank project

---
+++ src/main/java/com/workshop/zukerjava/ZukerjavaApplication.java
@@ -0,0 +1,12 @@
+package com.workshop.zukerjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZukerjavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZukerjavaApplication.class, args);
+    }
+}
