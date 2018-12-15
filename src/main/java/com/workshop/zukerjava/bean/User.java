b0ea1388f7d0a86f4d13b5a343da9d0ffb3563c3
hly <hongliangyi@sjtu.edu.cn>
2018-12-15 12:10:57 +0800

    modified bean/User

--- src/main/java/com/workshop/zukerjava/bean/User.java
+++ src/main/java/com/workshop/zukerjava/bean/User.java
@@ -1,17 +1,29 @@
 package com.workshop.zukerjava.bean;
 
-import java.io.Serializable;
+import org.springframework.data.mongodb.core.index.IndexDirection;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.io.Serializable;
 /**
  * 用户个人信息java bean
  */
+
+@Entity
+@Document(collection = "user")
 public class User implements Serializable {
 
+    @Id
     private String user_id;
+    @Indexed(unique = true, direction = IndexDirection.DESCENDING, dropDups = true)
 
     private String username;
 
     private String password;
+
+    private String email; //为了找回密码，加上邮箱
 
     private String avatarPath; //上传头像：图像路径
 
@@ -39,11 +51,28 @@ public class User implements Serializable {
         this.password = password;
     }
 
+    public String getEmail(){
+        return email;
+    }
+
+    public void setEmail(String email){
+        this.email = email;
+    }
+
     public String getAvatarPath() {
         return avatarPath;
     }
 
     public void setAvatarPath(String avatarPath) {
         this.avatarPath = avatarPath;
+    }
+
+
+    //参考https://github.com/shuangbofu/ustate/
+
+    @Override
+    public String toString() {
+        return "User [id=" + user_id + ", username=" + username + ", password="
+                + password + ", avatarPath=" + avatarPath + "]";
     }
 }
